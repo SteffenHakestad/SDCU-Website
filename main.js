ff73f709a2414afa7ba910910d111ec82718c824
SteffenHakestad <Hakestad.steffen@gmail.com>
2023-04-20 17:06:46 +0200

    Set up all pages. Continued work on home page.

--- main.js
+++ main.js
@@ -1,8 +1,25 @@
-let counter = 1;
-setInterval(function(){
-    document.getElementById('radio' + counter).checked = true;
-    counter++;
-    if(counter > 4){
-        counter = 1;
-    }
-})
+
+
+//checks the next radio button evey 7sec, causes the slider to change
+ let counter = 1;
+ setInterval(function(){
+     document.getElementById('radio' + counter).checked = true;
+     counter++;
+     if(counter > 4){
+         counter = 1;
+     }
+ }, 7000);
+
+
+// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+// window.onscroll = function() {scrollFunction()};
+
+// function scrollFunction() {
+//   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+//     document.getElementById("navbar").style.padding = "30px 10px";
+//     document.getElementById("logo").style.fontSize = "25px";
+//   } else {
+//     document.getElementById("navbar").style.padding = "80px 10px";
+//     document.getElementById("logo").style.fontSize = "35px";
+//   }
+// } 
