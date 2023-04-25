608628da600bfe2aa50140c089708247678544fb
SteffenHakestad <Hakestad.steffen@gmail.com>
2023-04-25 15:17:27 +0200

    Finished home page.
    Started working on responsiveness

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 
+addEventListener("DOMContentLoaded", () => {
+    document.getElementById("radio1").checked = true;
+});
+
+
 
 //checks the next radio button evey 7sec, causes the slider to change
- let counter = 1;
- setInterval(function(){
+let counter = 1;
+ const slideInterval = setInterval(function(){
      document.getElementById('radio' + counter).checked = true;
      counter++;
      if(counter > 4){
@@ -10,6 +15,13 @@
      }
  }, 7000);
 
+// If user manually selects a radio button, reset interval
+const radioButtons = document.querySelectorAll('input[type="radio"][name="radio-btn"]');
+radioButtons.forEach(radioButton => {
+  radioButton.addEventListener('change', () =>{
+    clearInterval(slideInterval);
+  });
+});
 
 // When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
 // window.onscroll = function() {scrollFunction()};
