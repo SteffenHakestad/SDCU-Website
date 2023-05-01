b4fffd9599ed3b8a54192dba6558aedc340b8757
SteffenHakestad <Hakestad.steffen@gmail.com>
2023-05-01 15:38:52 +0200

    Finished responsive navbar

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ addEventListener("DOMContentLoaded", () => {
 });
 
 
-
 //checks the next radio button evey 7sec, causes the slider to change
 let counter = 1;
  const slideInterval = setInterval(function(){
@@ -23,11 +22,6 @@ radioButtons.forEach(radioButton => {
   });
 });
 
-const toggleButton = document.getElementsByClassName("toggle-button")[0];
-const navbarLinks = document.getElementsByClassName("navbar-links")[0];
-toggleButton.addEventListener("click", () => {
-    navbarLinks.classList.toggle("active");
-});
 
 
 
@@ -36,15 +30,3 @@ toggleButton.addEventListener("click", () => {
 
 
 
-// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
-// window.onscroll = function() {scrollFunction()};
-
-// function scrollFunction() {
-//   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-//     document.getElementById("navbar").style.padding = "30px 10px";
-//     document.getElementById("logo").style.fontSize = "25px";
-//   } else {
-//     document.getElementById("navbar").style.padding = "80px 10px";
-//     document.getElementById("logo").style.fontSize = "35px";
-//   }
-// } 
---
+++ navbar.js
@@ -0,0 +1,37 @@
+//Gets elements needed to toggle between colors on hover.
+const dropdownButton = document.getElementById('dropdown-button');
+const blueArrow = document.getElementById('blue-arrow');
+const redArrow = document.getElementById('red-arrow');
+
+//Changes the color of the arrow on hover.
+dropdownButton.addEventListener('mouseover', () => {
+  blueArrow.style.display = 'none';
+  redArrow.style.display = 'inline-block';
+});
+
+dropdownButton.addEventListener('mouseout', () => {
+  blueArrow.style.display = 'inline-block';
+  redArrow.style.display = 'none';
+});
+
+
+const toggleButton = document.getElementsByClassName("toggle-button")[0];
+const navbarLinks = document.getElementsByClassName("navbar-links")[0];
+toggleButton.addEventListener("click", () => {
+    navbarLinks.classList.toggle("active");
+});
+
+//When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").style.height = "4rem";
+    document.getElementById("logo-image").style.width = "3rem";
+    document.getElementById("brand-title").style.setProperty("font-size", "3rem");
+  } else {
+    document.getElementById("navbar").style.height = "7rem";
+    document.getElementById("logo-image").style.width = "5rem";
+    document.getElementById("brand-title").style.setProperty("font-size", "4rem");
+  }
+} 
