9cce65b3a3413f4181dd1a6d0f81fadd14efbf45
SteffenHakestad <Hakestad.steffen@gmail.com>
2023-05-11 16:46:26 +0200

    Finished all Case History examples

--- main.js
+++ main.js
@@ -3,16 +3,23 @@ addEventListener("DOMContentLoaded", () => {
     document.getElementById("radio1").checked = true;
 });
 
-
-//checks the next radio button evey 7sec, causes the slider to change
+//Function to change sliding images
 let counter = 1;
- const slideInterval = setInterval(function(){
-     document.getElementById('radio' + counter).checked = true;
-     counter++;
-     if(counter > 4){
-         counter = 1;
-     }
- }, 7000);
+function slideInterValFunc(){
+  document.getElementById('radio' + counter).checked = true;
+  counter++;
+  if(counter > 4){
+      counter = 1;
+  }
+}
+let intervalId;
+//Start interval, gets called from body onload of index.html
+function startInterval(){
+  intervalId = setInterval(slideInterValFunc, 7000);
+}
+function stopInterval(){
+  clearInterval(intervalId);
+}
 
 // If user manually selects a radio button, reset interval
 const radioButtons = document.querySelectorAll('input[type="radio"][name="radio-btn"]');
@@ -22,11 +29,12 @@ radioButtons.forEach(radioButton => {
   });
 });
 
-
-
-
-
-
-
-
-
+//Function to download PDF
+function downloadPDF(x) {
+  var link = document.createElement('a');
+  link.href = 'Resources/SDCU-Case-History-' + x + '.pdf';
+  link.target = '_blank';
+  //Uncomment the line below to also download the PDF
+  //link.download = 'SDCU-Case-History-' + x + '.pdf';
+  link.dispatchEvent(new MouseEvent('click'));
+}
